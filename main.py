bf51645fa7fcdbb9fca3047b1191f910bab05e18
uulukbek <ulanovulukbek2@gmail.com>
2023-01-23 20:40:24 +0600

    types

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ bot = telebot.TeleBot(config.TOKEN)
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    inf = 'Здравствуйте,Я бот Jellyfish.\n Здесь вы можете:\n 1)получить информацию о продукте \n2) Сообщить об ошибке'
+    inf = 'Здравствуйте,Я бот Jellyfish.\n Здесь вы можете:\n 1)получить информацию о продукте 📫 \n2) Сообщить об ошибке 📝'
     markup = types.ReplyKeyboardMarkup()
     item1 = types.KeyboardButton('Товары')
     item2 = types.KeyboardButton('Проблемы')
@@ -28,10 +28,30 @@ def start_text(message):
     elif message.text =='Проблемы':
         email = 'ulanovulukbek2@gmail.com'
         phone = 'https://wa.me/996703845973'
-        bot.send_message(message.chat.id, f"Если у вас возникли проблемы с работой напишите нам! {email}\n {phone}" )
+        bot.send_message(message.chat.id, f"Если у вас возникли проблемы с работой напишите нам! 📝\n{email}\n {phone}" )
+    elif message.text == 'Книги':
+        markup = types.ReplyKeyboardMarkup()
+        item1 = types.KeyboardButton('Анти утопия')
+        item2 = types.KeyboardButton('Утопия')
+        item3 = types.KeyboardButton('Роман')
+        item4 = types.KeyboardButton('Драма')
+        item5 = types.KeyboardButton('Назад в меню')
+        markup.row(item1, item2)
+        markup.row(item3, item4, item5)
+        bot.send_message(message.chat.id, 'Выберите жанр:', reply_markup=markup)
+    elif message.text == 'Анти утопия':
+        bot.send_message(message.chat.id, 'Здесь могла бы быть ваша реклама')
+    elif message.text == 'Утопия':
+        bot.send_message(message.chat.id, 'Здесь могла бы быть ваша реклама')
+    elif message.text == 'Роман':
+        bot.send_message(message.chat.id, 'Здесь могла бы быть ваша реклама')
+    elif message.text == 'Драма':
+        bot.send_message(message.chat.id, 'Здесь могла бы быть ваша реклама')
+
     else: 
-        bot.send_message(message.chat.id, 'Вы выбрали не существующий параметр. \n Попробуйте снова!')
+        bot.send_message(message.chat.id, 'Вы выбрали не существующий параметр \n Попробуйте снова!')
 
 
+    
 if __name__=='__main__':
     bot.polling(True)
